4d12a251df19ccafe226245f91d7fcce45fce0ed
davidshiao55 <davidshiao55@gmail.com>
2025-11-23 16:52:30 +0800

    Add segment register to the bootloader

--- boot.asm
+++ boot.asm
@@ -1,7 +1,19 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
+jmp 0x7C0: start ; Set code segment to 0x7C0
+
 start:
+    cli ; Disable Interrupts for critical setup
+    ; manually set up segment registers, incase BIOS set them differently the code won't work as expected
+    mov ax, 0x07C0  ; Can't directly move to segment registers
+    mov ds, ax
+    mov es, ax
+    ; Stack is just below the bootloader and grows downwards to 0x0000
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7C00 
+    sti ; Enables Interrupts
     mov si, message
     call print
     jmp $   ; infinite loop
@@ -9,7 +21,7 @@ start:
 print:
     mov bx, 0
 .loop:
-    lodsb   ; load byte at SI into AL and increment SI
+    lodsb   ; load byte at DS:SI into AL and increment SI -> ds(0x7C0) * 16 + offset
     cmp al, 0
     je .done
     call print_char
