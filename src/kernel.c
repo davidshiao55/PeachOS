875df192f04ab11eb827523feecc071380f4eb3c
davidshiao55 <davidshiao55@gmail.com>
2025-11-26 01:18:57 +0800

    Implementing disk driver

--- src/kernel.c
+++ src/kernel.c
@@ -1,4 +1,5 @@
 #include "kernel.h"
+#include "disk/disk.h"
 #include "idt/idt.h"
 #include "io/io.h"
 #include "memory/heap/kheap.h"
@@ -79,17 +80,11 @@ void kernel_main()
     // Switch to kernel paging chunk
     paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
 
-    char *ptr = kzalloc(4096);
-    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void *)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITABLE);
-
     // Enable paging
     enable_paging();
 
-    char *ptr2 = (char *)0x1000;
-    ptr2[0] = 'A';
-    ptr2[1] = 'B';
-    print(ptr2);
-    print(ptr);
+    char buf[512];
+    disk_read_sector(0, 1, buf);
 
     // Enable interrupts. Only after IDT is initialized and paging is enable, or system may crash
     enable_interrupts();
