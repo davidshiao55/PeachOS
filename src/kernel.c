19bf6a10dcbb3b6715ac57b142ad170136e6dec0
davidshiao55 <davidshiao55@gmail.com>
2025-11-26 03:18:06 +0800

    Implementing disk streamer

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,6 @@
 #include "kernel.h"
 #include "disk/disk.h"
+#include "disk/streamer.h"
 #include "fs/pparser.h"
 #include "idt/idt.h"
 #include "io/io.h"
@@ -83,7 +84,10 @@ void kernel_main()
     // Enable interrupts. Only after IDT is initialized and paging is enable, or system may crash
     enable_interrupts();
 
-    struct path_root *root_path = pathparser_parse("0:/bin/shell.exe", NULL);
-    if (root_path) {
+    struct disk_stream *stream = diskstreamer_new(0);
+    diskstreamer_seek(stream, 0x201);
+    unsigned char c = 0;
+    diskstreamer_read(stream, &c, 1);
+    while (1) {
     }
 }
