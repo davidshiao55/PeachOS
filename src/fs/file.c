70eeaf533fd99ff0aff6b66f74d15292c99acc65
davidshiao55 <davidshiao55@gmail.com>
2025-11-30 01:08:47 +0800

    Implementing VFS fopen

--- src/fs/file.c
+++ src/fs/file.c
@@ -1,10 +1,12 @@
 #include "file.h"
 #include "config.h"
+#include "disk/disk.h"
 #include "fat/fat16.h"
 #include "kernel.h"
 #include "memory/heap/kheap.h"
 #include "memory/memory.h"
 #include "status.h"
+#include "string/string.h"
 
 struct filesystem *filesystems[PEACHOS_MAX_FILESYSTEMS];
 struct file_descriptor *file_descriptors[PEACHOS_MAX_DESCRIPTORS];
@@ -92,7 +94,70 @@ struct filesystem *fs_resolve(struct disk *disk)
     return fs;
 }
 
-int fopen(const char *filename, const char *mode)
+FILE_MODE file_get_mode_by_string(const char *str)
 {
-    return -EIO;
+    FILE_MODE mode = FILE_MODE_INVALID;
+    if (strncmp(str, "r", 1) == 0) {
+        mode = FILE_MODE_READ;
+    } else if (strncmp(str, "w", 1) == 0) {
+        mode = FILE_MODE_WRITE;
+    } else if (strncmp(str, "a", 1) == 0) {
+        mode = FILE_MODE_APPEND;
+    }
+    return mode;
+}
+
+int fopen(const char *filename, const char *mode_str)
+{
+    int res = 0;
+    struct path_root *root_path = pathparser_parse(filename, NULL);
+    if (!root_path) {
+        res = -EINVARG;
+        goto out;
+    }
+    // Check if open on root path
+    if (!root_path->first) {
+        res = -EINVARG;
+        goto out;
+    }
+
+    // Chkeck disk exist
+    struct disk *disk = disk_get(root_path->drive_no);
+    if (!disk) {
+        res = -EIO;
+        goto out;
+    }
+
+    // Check if filesystem exist
+    if (!disk->filesystem) {
+        res = -EIO;
+        goto out;
+    }
+
+    FILE_MODE mode = file_get_mode_by_string(mode_str);
+    if (mode == FILE_MODE_INVALID) {
+        res = -EINVARG;
+        goto out;
+    }
+
+    void *descriptor_private_data = disk->filesystem->open(disk, root_path->first, mode);
+    if (ISERR(descriptor_private_data)) {
+        res = ERROR_I(descriptor_private_data);
+        goto out;
+    }
+
+    struct file_descriptor *desc = 0;
+    res = file_new_descriptor(&desc);
+    if (res < 0) {
+        goto out;
+    }
+    desc->filesystem = disk->filesystem;
+    desc->private_data = descriptor_private_data;
+    desc->disk = disk;
+    res = desc->index;
+out:
+    // fopen shouldn't return negative value
+    if (res < 0)
+        res = 0;
+    return res;
 }
--- src/string/string.c
+++ src/string/string.c
@@ -1,5 +1,12 @@
 #include "string.h"
 
+char tolower(char c)
+{
+    if (c >= 65 && c <= 90)
+        c += 32;
+    return c;
+}
+
 int strlen(const char *ptr)
 {
     int i = 0;
@@ -18,6 +25,45 @@ int strnlen(const char *ptr, int max)
             break;
     }
     return i;
+}
+
+int strnlen_terminator(const char *str, int max, char terminator)
+{
+    int i = 0;
+    for (i = 0; i < max; i++) {
+        if (str[i] == 0 || str[i] == terminator)
+            break;
+    }
+    return i;
+}
+
+int istrncmp(const char *str1, const char *str2, int n)
+{
+    unsigned char u1, u2;
+    while (n-- > 0) {
+        u1 = *(unsigned char *)str1;
+        u2 = *(unsigned char *)str2;
+        if (u1 != u2 && tolower(u1) != tolower(u2))
+            return u1 - u2;
+        if (u1 == '\0')
+            return 0;
+    }
+    return 0;
+}
+
+int strncmp(const char *str1, const char *str2, int n)
+{
+    unsigned char u1, u2;
+    while (n-- > 0) {
+        u1 = *(unsigned char *)str1;
+        u2 = *(unsigned char *)str2;
+        if (u1 != u2)
+            return u1 - u2;
+
+        if (u1 == '\0')
+            return 0;
+    }
+    return 0;
 }
 
 char *strcpy(char *dest, const char *src)
