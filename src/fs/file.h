ee87ce89a22a89ea2c4e7a9308fe23fa595446ad
davidshiao55 <davidshiao55@gmail.com>
2025-11-30 16:29:35 +0800

    Implementing VFS fclose

--- src/fs/file.h
+++ src/fs/file.h
@@ -32,6 +32,7 @@ typedef int (*FS_RESOLVE_FUNCTION)(struct disk *disk);
 typedef int (*FS_READ_FUNCTION)(struct disk *disk, void *private, uint32_t size, uint32_t nmemb, char *out);
 typedef int (*FS_SEEK_FUNCTION)(void *private, uint32_t offset, FILE_SEEK_MODE seek_mode);
 typedef int (*FS_STAT_FUNCTION)(struct disk *disk, void *private, struct file_stat *stat);
+typedef int (*FS_CLOSE_FUNCTION)(void *private);
 
 struct filesystem {
     // Filesystem should return zero from resolve if the provided disk is using its filesystem
@@ -40,6 +41,7 @@ struct filesystem {
     FS_READ_FUNCTION read;
     FS_SEEK_FUNCTION seek;
     FS_STAT_FUNCTION stat;
+    FS_CLOSE_FUNCTION close;
 
     char name[20];
 };
@@ -70,5 +72,6 @@ void fs_insert_filesystem(struct filesystem *filesystem);
 struct filesystem *fs_resolve(struct disk *disk);
 int fseek(int fd, int offset, FILE_SEEK_MODE whence);
 int fstat(int fd, struct file_stat *stat);
+int fclose(int fd);
 
 #endif
