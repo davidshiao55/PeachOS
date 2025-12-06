b588917996ea3a2b57a2e66b9ef953325b914bd7
davidshiao55 <davidshiao55@gmail.com>
2025-12-06 15:20:23 +0800

    Getting key from user land

--- programs/blank/blank.asm
+++ programs/blank/blank.asm
@@ -3,11 +3,20 @@ section .asm
 global _start
 
 _start:
+    call getkey ; Wait for keypresh and output a message
     push message
-    mov eax, 1 ; Command print
+    mov eax, 1
     int 0x80
     add esp, 4
+
     jmp $
+
+getkey:
+    mov eax, 2  ; Command getkey
+    int 0x80
+    cmp eax, 0x00
+    je getkey
+    ret
 
 section .data
 message: db 'I can talk with the kerenl!', 0
